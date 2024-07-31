ae99939661111aac42d6a9f52ecf6243b28397ab
xiaofengzi <asxiaofengzi@gmail.com>
2024-07-31 12:59:39 +0800

    setup

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ GIT_REPO_TINY_WEBSERVER="https://github.com/asxiaofengzi/TinyWebServer.git"
 GIT_REPO_LEARN_TINY_WEBSERVICE="https://github.com/asxiaofengzi/learn_TinyWebservice.git"
 FRP_DOWNLOAD_URL="https://github.com/fatedier/frp/releases/download/v0.59.0/frp_0.59.0_linux_amd64.tar.gz"
 ORACLE_GZ_CLIENT_BOT_URL="https://github.com/semicons/java_oci_manage/releases/latest/download/gz_client_bot.tar.gz"
+FILE_URL="https://github.com/asxiaofengzi/file.git"
 
 # 配置git
 echo "配置git..."
@@ -14,11 +15,12 @@ git config --global user.email "asxiaofengzi@gmail.com"
 
 # 创建c++_project目录并克隆项目
 echo "克隆项目到c++_project目录..."
-mkdir c++_project
-cd c++_project
+mkdir /root/c++_project
+cd /root/c++_project
 git clone "$GIT_REPO_TINY_WEBSERVER"
 git clone "$GIT_REPO_LEARN_TINY_WEBSERVICE"
 cd /root
+git clone "$FILE_URL"
 
 # 配置frp，设置成开机自启动服务
 echo "配置frp服务..."
@@ -28,7 +30,8 @@ yum install wget
 wget "$FRP_DOWNLOAD_URL"
 tar -zxvf frp_0.59.0_linux_amd64.tar.gz
 cd frp_0.59.0_linux_amd64
-
+mv /root/file/setup_frps_service.sh /root/frp/frp_0.59.0_linux_amd64
+chmod +x setup_frps_service.sh
 # 执行frp服务设置脚本，假设 setup_frps_service.sh 脚本内容是正确的
 # 注意：确保 setup_frps_service.sh 脚本具有执行权限
 ./setup_frps_service.sh
@@ -48,6 +51,8 @@ bash sh_client_bot.sh
 vi /root/oracle/client_config
 
 # 运行oracle.sh脚本，假设该脚本存在于当前目录且具有执行权限
+mv /root/file/oracle.sh /root/oracle
+chmod +x /root/oracle/oracle.sh
 ./oracle.sh
 
 # 再次运行刷机脚本
